4c32ac0e9ced6e47727b73b4a9ac7b7c3269ac9c
Wes Kendall <wes@counsyl.com>
2016-02-21 21:44:25 -0800

    more verbose output

--- integration_test.sh
+++ integration_test.sh
@@ -4,7 +4,9 @@ if [ -n SWIFT_TEST_USERNAME ]; then
     echo "Installing hpc smoketest for integration test"
     pip install --no-deps hpc-smoketest==1.0.0
     echo "Running integration test"
-    exit_code=`test-swift | head -n 1 | cut -f1 -d' '`
+    test_out=`test-swift`
+    echo $test_out
+    exit_code=`echo $test_out | head -n 1 | cut -f1 -d' '`
     exit $exit_code
 else
     echo "Ignoring swift integration tests. To run the tests, set the SWIFT_TEST_USERNAME and SWIFT_TEST_PASSWORD environment variables"
