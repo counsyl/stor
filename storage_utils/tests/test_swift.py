bea4a1c07b530a14340bc5bf0e35a08bf5c8593b
Jeff Tratner <jtratner@counsyl.com>
2016-02-24 15:56:15 -0800

    Default inline to true (expected within Counsyl)
    Sem-Ver: api-break

--- storage_utils/tests/test_swift.py
+++ storage_utils/tests/test_swift.py
@@ -384,13 +384,16 @@ class TestTempURL(SwiftTestCase):
     @mock.patch('storage_utils.swift.auth_url', 'https://swift.com/auth/v1/')
     def test_success(self):
         temp_url = SwiftPath('swift://tenant/container/obj').temp_url()
-        expected = 'https://swift.com/v1/tenant/container/obj?temp_url_sig=3b1adff9452165103716d308da692e6ec9c2d55f&temp_url_expires=1456229100'  # nopep8
+        expected = 'https://swift.com/v1/tenant/container/obj?temp_url_sig=3b1adff9452165103716d308da692e6ec9c2d55f&temp_url_expires=1456229100&inline'  # nopep8
         self.assertEquals(temp_url, expected)
 
     @freezegun.freeze_time('2016-02-23 12:00:00')
     @mock.patch('storage_utils.swift.temp_url_key', 'temp_key')
     @mock.patch('storage_utils.swift.auth_url', 'https://swift.com/auth/v1/')
     def test_success_w_inline(self):
+        temp_url = SwiftPath('swift://tenant/container/obj').temp_url(inline=False)
+        expected = 'https://swift.com/v1/tenant/container/obj?temp_url_sig=3b1adff9452165103716d308da692e6ec9c2d55f&temp_url_expires=1456229100'  # nopep8
+        self.assertEquals(temp_url, expected)
         temp_url = SwiftPath('swift://tenant/container/obj').temp_url(inline=True)
         expected = 'https://swift.com/v1/tenant/container/obj?temp_url_sig=3b1adff9452165103716d308da692e6ec9c2d55f&temp_url_expires=1456229100&inline'  # nopep8
         self.assertEquals(temp_url, expected)
