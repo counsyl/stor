564941772b066f2cfae2477b66ca5df1dd170851
Jeff Tratner <jtratner@counsyl.com>
2016-03-04 09:56:44 -0800

    Ensure container exists before checks

--- storage_utils/tests/test_integration.py
+++ storage_utils/tests/test_integration.py
@@ -239,10 +239,18 @@ class SwiftIntegrationTest(BaseIntegrationTest):
     def test_is_methods(self):
         container = self.test_container
         container = self.test_container
+        file_with_prefix = storage_utils.join(container, 'analysis.txt')
+
+        # ensure container is crated but empty
+        sentinel = storage_utils.join(container, 'sentinel')
+        with storage_utils.open(sentinel, 'w') as fp:
+            fp.write('blah')
+        storage_utils.remove(sentinel)
         self.assertTrue(storage_utils.isdir(container))
         self.assertFalse(storage_utils.isfile(container))
         self.assertTrue(storage_utils.exists(container))
-        file_with_prefix = storage_utils.join(container, 'analysis.txt')
+        self.assertFalse(storage_utils.listdir(container))
+
         folder = storage_utils.join(container, 'analysis')
         subfolder = storage_utils.join(container, 'analysis', 'alignments')
         file_in_folder = storage_utils.join(container, 'analysis', 'alignments',
