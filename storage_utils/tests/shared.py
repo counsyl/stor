4db9ab2a0333f837b2b53f7f5dfa01ed90d39cd8
Jeff Tratner <jtratner@counsyl.com>
2016-03-09 23:17:39 -0800

    commit shared test helpers

---
+++ storage_utils/tests/shared.py
@@ -0,0 +1,7 @@
+def assert_same_data(fp1, fp2):
+    actual_data = fp1.read(100)
+    expected_data = fp2.read(100)
+    while (expected_data or actual_data):
+        assert actual_data == expected_data
+        actual_data = fp1.read(100)
+        expected_data = fp2.read(100)
