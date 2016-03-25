7980cec6be95ae9e877894b8a6cff7c291a04589
Wes Kendall <wes@counsyl.com>
2016-03-24 21:35:55 -0400

    Added fnmatch method
    Sem-Ver: feature

--- storage_utils/base.py
+++ storage_utils/base.py
@@ -1,6 +1,7 @@
 from storage_utils import utils
 
 import errno
+import fnmatch
 import glob
 import os
 import ntpath
@@ -544,3 +545,19 @@ class FileSystemPath(Path):
             elif isdir:
                 for f in child.walkfiles(pattern, errors):
                     yield f
+
+    def fnmatch(self, pattern, normcase=None):
+        """ Return ``True`` if `self.name` matches the given `pattern`.
+        `pattern` - A filename pattern with wildcards,
+            for example ``'*.py'``. If the pattern contains a `normcase`
+            attribute, it is applied to the name and path prior to comparison.
+        `normcase` - (optional) A function used to normalize the pattern and
+            filename before matching. Defaults to :meth:`self.module`, which defaults
+            to :meth:`os.path.normcase`.
+        .. seealso:: :func:`fnmatch.fnmatch`
+        """
+        default_normcase = getattr(pattern, 'normcase', self.path_module.normcase)
+        normcase = normcase or default_normcase
+        name = normcase(self.name)
+        pattern = normcase(pattern)
+        return fnmatch.fnmatchcase(name, pattern)
