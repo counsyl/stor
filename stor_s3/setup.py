023aa5bcb072809cca273cbe822c9cb7a8a1c82c
Anuj Kumar <akumar@counsyl.com>
2018-11-15 14:12:00 -0800

    removing comments

--- stor_s3/setup.py
+++ stor_s3/setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
-# import sys
 
 setup(
     pbr=True,
     setup_requires=['pbr'],
-    # entry_points="""
-    #     [stor.providers.{}]
-    #     s3 = stor_s3:find_cls_for_path
-    # """.format(sys.version_info[0])
 )
