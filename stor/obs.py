67fd9f98dadcc9875cfc3acc29bcfbaaa5f2e9b1
Jeff Tratner <jeffrey.tratner@gmail.com>
2018-11-29 23:32:04 -0800

    Update stor/obs.py
    Co-Authored-By: anujkumar93 <anujkumar.maverick@gmail.com>

--- stor/obs.py
+++ stor/obs.py
@@ -80,7 +80,7 @@ class OBSPath(Path):
         windows paths created with ``stor.Path``.
 
         Args:
-            self (path|str): The source directory to copy from
+            self (Path|str): The source directory to copy from
             dest (path|str): The destination file or directory.
             swift_retry_options (dict): Optional retry arguments to use for swift
                 upload or download. View the
