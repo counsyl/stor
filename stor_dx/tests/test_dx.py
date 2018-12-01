b4b5eea14292f5d32673f6042ca1549312544721
Anuj Kumar <akumar@counsyl.com>
2018-12-01 13:40:17 -0800

    refining tests

--- stor_dx/tests/test_dx.py
+++ stor_dx/tests/test_dx.py
@@ -10,8 +10,8 @@ import mock
 
 from stor import exceptions
 from stor import Path
-from stor_dx import utils
 from stor.tests.shared_obs import SharedOBSFileCases
+from stor_dx import utils
 from stor_dx.dx import DXPath
 from stor_dx.test import DXTestCase
 import stor
