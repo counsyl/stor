bb6d5da5e870b4c3d9268714f3a3d3165be3f473
Jeff Tratner <jeffrey.tratner@gmail.com>
2019-08-05 10:42:18 -0700

    re-merge together tests so less boilerplate

--- stor/tests/shared_obs.py
+++ stor/tests/shared_obs.py
@@ -208,26 +208,15 @@ class SharedOBSFileCases(object):
         mock_write_object.assert_called_with(self.normal_path, b'happy-go_lucky')
         self.assertEquals(len(mock_write_object.call_args_list), 1)
 
-    def test_readable(self):
+    def test_readable_writable_seekable(self):
         pth = self.normal_path
         read_obj = pth.open(mode='r')
         self.assertTrue(read_obj.readable())
-
-        write_obj = stor.open(stor.join(self.drive, 'B/C/obj'), 'w')
-        self.assertFalse(write_obj.readable())
-
-    def test_writable(self):
-        pth = self.normal_path
-        read_obj = pth.open(mode='r')
         self.assertFalse(read_obj.writable())
-
-        write_obj = stor.open(stor.join(self.drive, 'B/C/obj'), 'w')
-        self.assertTrue(write_obj.writable())
-
-    def test_seekable(self):
-        pth = self.normal_path
-        read_obj = pth.open(mode='r')
         self.assertTrue(read_obj.seekable())
 
         write_obj = stor.open(stor.join(self.drive, 'B/C/obj'), 'w')
+        self.assertFalse(write_obj.readable())
+        self.assertTrue(write_obj.writable())
         self.assertTrue(write_obj.seekable())
+       
