9fe1f827275a9b7ec56eadf35fdc635bb17daacd
Ying Wu <ying-w@users.noreply.github.com>
2020-08-14 17:43:34 -0700

    revert "removed code that does utf8 conversion since done by dxpy"
    This reverts commit 34cc1f70691dec154b0f93ce34f2828d37f1f8c2.

--- stor/dx.py
+++ stor/dx.py
@@ -848,6 +848,7 @@ class DXPath(OBSPath):
 
     def read_object(self, mode='r'):
         """Reads an individual object from DX.
+        Note dxpy for Py3 automatically decodes the DXFile.read using utf-8.
 
         Returns:
             bytes: the raw bytes from the object on DX.
@@ -859,6 +860,8 @@ class DXPath(OBSPath):
                                    mode=mode)
         with _wrap_dx_calls():
             result = file_handler.read()
+        # TODO (akumar): allow other encoding after update of encoding in dxpy for Py3
+        result = result.encode('utf-8')  # dxpy for py3 already decodes the data with 'utf-8'
         return result
 
     def write_object(self, content, **kwargs):
